45e45f0d192197f23cea8025f6142b74d836a12c
ersr89 <ersr1@aluno.ifal.edu.br>
2022-09-12 20:14:59 +0000

    divisao

---
+++ api-mvc/src/main/java/Divisao.java
@@ -0,0 +1,12 @@
+package app;
+
+import io.jooby.annotations.*;
+
+@Path("/divi/{op}/{op2}")
+public class Divisao {
+
+    @GET
+    public double divi_resultado(@PathParam double op ,@PathParam double op2) {
+         return op/op2
+    }
+}
