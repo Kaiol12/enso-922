53024a0d1677765cecc13afa268a5442c145b1d3
Josemauricioe <jmesn1@aluno.ifal.edu.br>
2022-09-28 20:17:34 +0000

    Soma_Teste

--- api-mvc/src/test/java/app/SomaTest.java
+++ api-mvc/src/test/java/app/SomaTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class somatest {
+public class SomaTest {
     
     Adicao adicao;
 
