ff7631cb1f3d265262a4f97088df0cad7323ab39
Josemauricioe <jmesn1@aluno.ifal.edu.br>
2022-09-30 00:19:13 +0000

    corrigindo

--- api-mvc/src/test/java/app/SomaTest.java
+++ api-mvc/src/test/java/app/SomaTest.java
@@ -1,7 +1,6 @@
 package app;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -28,13 +27,16 @@ public class SomaTest {
         double resultado = adicao.adicao_resultado(-20,10);
         assertEquals(-10, resultado);
     }
+
     @Test
     public void soma_com_nnumeros_grandes(){
         double resultado = adicao.adicao_resultado(10000,10000);
         assertEquals(20000, resultado);
-
-
     }
+
+
+
+    
 }
 
 
