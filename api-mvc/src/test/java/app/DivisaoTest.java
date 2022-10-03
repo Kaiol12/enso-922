f64603f5f8dd1ed1a0111d234cf6ac9b9795161a
ersr89 <ersr1@aluno.ifal.edu.br>
2022-10-03 19:02:43 +0000

    divisaoteste

---
+++ api-mvc/src/test/java/app/DivisaoTest.java
@@ -0,0 +1,39 @@
+package app;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.*;
+
+
+public class DivisaoTest {
+    
+    Divisao divisao;
+
+
+    @BeforeEach
+    public void beforeEach() {
+        divisao = new Divisao(); 
+    }
+
+    @Test
+    public void testCasoBase() {
+        double resultado = divisao.divi_resultado(2,2);
+        assertEquals(1, resultado);
+    }
+    @Test
+    public void numero_negativos() {
+        double resultado = divisao.divi_resultado(-4,(2));
+        assertEquals(-2, resultado);
+    }
+
+    @Test
+    public void Numeros_decima() {
+        double resultado = divisao.divi_resultado(1000,0.5);
+        assertEquals(2000, resultado)
+    }
+
+    
+}
+
+
+
+    
